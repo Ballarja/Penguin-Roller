23a705f5e44adc0cfc042c51cce4045cf8b70bc8
Ballarja <54514754+Ballarja@users.noreply.github.com>
2020-02-12 19:03:20 -0600

    Score and High Score Script added

---
+++ Scripts/HighScore.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour
+{
+    public Text score;
+    public Text highScore;
+
+    void Start()
+    {
+
+        highScore.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+    }
+
+    public void UpdateScore()
+    {
+        int number = Random.Range(1, 2000);
+        score.text = number.ToString();
+
+        if (number > PlayerPrefs.GetInt("HighScore", 0))
+        {
+            PlayerPrefs.SetInt("HighScore", number);
+            highScore.text = number.ToString();
+        }
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Scripts/Score.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    public Transform Ball;
+    public Text scoreText;
+    void Update()
+    {
+        scoreText.text = Ball.position.z.ToString("0");
+    }
+}
